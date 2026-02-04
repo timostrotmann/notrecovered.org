1f5a94564db6cf326f768b02f2af015498e8a24f
Timo Strotmann <timo@strotmann.me>
2026-02-04 16:10:02 +0100

    refactor GET function to use URL constructor for redirection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,3 +1,9 @@
-export async function GET() {
-	return Response.redirect('/en/rss.xml', 301);
+export async function GET(context) {
+	const target = new URL('/en/rss.xml', context.site);
+	return new Response(null, {
+		status: 301,
+		headers: {
+			Location: target.toString(),
+		},
+	});
 }
